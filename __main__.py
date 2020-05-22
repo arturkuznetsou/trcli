06ff7fa9263f95ad898aaf49ef97aa415ee009f1
Artur Kuznetsou <arturkuznetsou@pm.me>
2020-05-22 15:13:11 +0200

    Fixed syntax error.

--- __main__.py
+++ __main__.py
@@ -2,7 +2,10 @@ import getopt, sys, other
 import translate as tr
 gen_aud_file = True
 
-opts, args = getopt.getopt(sys.argv[1:], 'to:')
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'hto:')
+except Exception as error:
+    sys.exit("An error occured. " + str(error))
 
 if len(args) < 3:
     sys.exit('Too few positional arguments. trscli <format> <input language>-<output language> <file>.')
@@ -20,6 +23,14 @@ for arg, value in opts:
 
 
 
+
 pattern = other.gen_format(args[-3])
-trans = tr.gen_text(args[-1], langin, langout)
-tr.gen_audio_file(trans, langin, langout, pattern, fileout)
+try:
+    trans = tr.gen_text(args[-1], langin, langout)
+except Exception as error:
+    sys.exit(error)
+
+try:
+    tr.gen_audio_file(trans, langin, langout, pattern, fileout)
+except Exception as error:
+    sys.exit(error)
--- other.py
+++ other.py
@@ -3,19 +3,20 @@ defaultformat = [[False, 1, 1], [True, 1.5, 1]]
 def gen_format (formatstring):
     newret = []
     formats = formatstring.split('/')
+    if len(formats) < 1:
+            print('Format specified incorrectly. Using default format.')
+            return defaultformat
     for formt in formats:
-        newformt = []
-        tpm = formt.split(',')
-        if len(tpm) != 3:
-            sys.exit('Output format specified incorrectly')
-        if tpm[0] == 't':
-            newformt.append(True)
-        else:
-            newformt.append(False)
         try:
-            newformt.append(1/float(tpm[1]))
-            newformt.append(float(tpm[2]))
-            newret.append(newformt)
+            newformt = []
+            tpm = formt.split(',')
+            if tpm[0] == 't':
+                newformt.append(True)
+            else:
+                newformt.append(False)
+                newformt.append(1/float(tpm[1]))
+                newformt.append(float(tpm[2]))
+                newret.append(newformt)
         except:
             print('Format specified incorrectly. Using default format.')
             return defaultformat
